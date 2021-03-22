5e22cdbf7ae1721d0303210d533ec93aa10cc9e2
Khotyn Huang <hting1@gmail.com>
2021-03-22 16:11:38 +0800

    First commit

---
+++ src/main/java/com/example/demo/SampleController.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.demo;
+
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
+import com.alipay.sofa.runtime.api.annotation.SofaReferenceBinding;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author khotyn
+ * @version v0.1 2021.03.22
+ */
+@Controller
+public class SampleController {
+    @Autowired
+    @SofaReference(interfaceType = SampleService.class, binding = @SofaReferenceBinding(bindingType = "bolt"), jvmFirst = false)
+    private SampleService sampleService;
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return sampleService.hello();
+    }
+}
